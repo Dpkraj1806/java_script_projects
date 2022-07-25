fb52e4086d681d4cada3913bfb05b4de045abf1d
Deepak Raj <66163072+Dpkraj1806@users.noreply.github.com>
2022-07-25 07:42:17 -0700

    Add files via upload

---
+++ Classes/links.js
@@ -0,0 +1,95 @@
+const link=[
+    {"class":"Session1","url":"https://youtu.be/iPC7CPF2McY"},
+    {"class":"Session2","url":"https://www.youtube.com/watch?v=VvL0z75Dl8w"},
+    {"class":"Session3","url":"https://youtu.be/A7SJzgtlG_o"},
+    {"class":"Session4","url":"https://youtu.be/Ao-47b3PCw4"},
+    {"class":"Session5","url":"https://youtu.be/5-f6TbAcEXw"},
+    {"class":"Session6","url":""},
+    // {"class":"Session7","url":""}
+    // {"class":"Session8","url":""}
+    // {"class":"Session9","url":""}
+    // {"class":"Session10","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+    // {"class":"Session","url":""}
+] 
+// 6. https://youtu.be/uYnOZ7Vs5PE
+// 7. https://youtu.be/TM-D9IozCDQ
+// 8. https://youtu.be/Vz-ccbzTn8E
+// 9. https://youtu.be/zYB182IOYig
+// 10.https://youtu.be/jWFqVCvlphY
+// 11.https://youtu.be/aQ9LqnW82Js
+// 12.https://youtu.be/-Y7-fNDL-ZE
+// 13.https://youtu.be/qJN_fSzlrW0
+// 14.https://youtu.be/64F6KjPPjcA
+// 15.https://youtu.be/DE7TJVf5p78
+// 16.https://youtu.be/Ls2kBTvFtsk
+// 17.https://youtu.be/K_tmePsBJU4
+// 18.https://youtu.be/AQDDEdfX5GA
+// 19.https://youtu.be/6N_POwFqnaY
+// 20.https://youtu.be/5IdjjR9LUUU
+// 21.https://youtu.be/qucpwzy_PXo
+// 22.https://youtu.be/hpXKjWlf1FE
+// 23.https://youtu.be/nUacKPM1u4g
+// 24.https://youtu.be/7Ez5Iisyd4s
+// 25.https://youtu.be/kCWN8HK8S8E
+// 26.https://youtu.be/94JrdpfWPEY
+// 27.https://youtu.be/1S2qG2TqxQo
+// 28.https://youtu.be/zFDk995EaDI
+// 29.https://youtu.be/CwEiaRVPwvE
+// 30.https://youtu.be/tLnsVDVDmlE
+// 31.https://youtu.be/cDI8WDVj_vk
+// 32.https://youtu.be/mIx8HNIagZU
+// 33.https://youtu.be/fpqJwf62ntg
+// 34.https://youtu.be/-kbjZahTfKk
+// 35.https://youtu.be/wxWQJUG5P-4
+// 36.https://youtu.be/d1d1V2UoFmM
+// 37.https://youtu.be/WaIArrR9nVE
+// 38.https://youtu.be/NP1lE4Gh4zY
+// 39.https://youtu.be/QYOpD-eprcc
+// 40.https://youtu.be/BSqelRmQXbU
+// 41.https://youtu.be/O2h6UvkcwFU
+// 42.https://youtu.be/ueEUZFG08u4
+// 43.https://youtu.be/dsDaFtaFHyw
+// 44.https://youtu.be/bF2lPEfG8nA
+// 45.https://youtu.be/u_t_5YRbvLc
+// 46.https://youtu.be/HxSjORaBiBw
+// 47.https://youtu.be/Sh3NHHcxnyI
+// 48.https://youtu.be/x4rkanOpGJQ
+// 49.https://youtu.be/wVdW9AmtnT8
+// 50.https://youtu.be/r_bOGKc2_ew
+// 51.Session01: https://youtu.be/bpMmGaddu04
+// 52.Session02: https://youtu.be/uL-xZOqY_gU
+// 53.Session03: https://youtu.be/i2bQUKBPRR8
+// 54.Session04: https://youtu.be/ZLoICUcDzb8
+// 55.Session05: https://youtu.be/vi5EevOlNHo
+// 56.Session06: https://youtu.be/TigfaWPvkO8
+// 57.Session07: https://youtu.be/dYmdurLxMAo
+// 58.Session08: https://youtu.be/48OCBrHGOw4
+// 59.Session09: https://youtu.be/ZPeFKy1aMzA
+// 60.Session10: https://youtu.be/JAwakxCq63s   Mock 1
+// 61.Session11: https://youtu.be/YBddPCxSeps   React start call back,timeout
+// 62.Session12: https://youtu.be/AIwZx2QK-Y4   promises
+// 63.Session13: https://youtu.be/B53g1bvqu3w   ES6,node installation, import and export,spread operator,
+// 64.Session14: https://youtu.be/tKQGyS2H-xw
+// 65.Session15: https://youtu.be/X_qzANxf8vY
+// 66.Session16: https://youtu.be/8IzzLwx3EEQ
+// 67.Session17: https://youtu.be/uwP_mnsJuf4
+// 68.Session18: https://youtu.be/W7m0mu3zdKQ
+// 69.Session19: https://youtu.be/r7G4px_juyU
+// 70.Session20: https://youtu.be/KzwFfDnf-og
+// 71.Session21: https://youtu.be/M1Svb95PYwk
+// 72.Session22: https://youtu.be/C1RhpS37EHQ
+// 73.Session23: https://youtu.be/WcWa6sEf0IQ
+// 74.Session24: https://youtu.be/j7P1N6yNygA  : Topic: Forms
+// 75.Session25: https://youtu.be/e1IuEDiUrKE
+// 76.Session26: https://youtu.be/eJqLfrbH_h0
+// 77.Session27: https://youtu.be/xkZovAzxAsA
---
+++ Classes/main.js
@@ -0,0 +1,102 @@
+var link=[
+    {"class":"Session1","url":"https://youtu.be/iPC7CPF2McY"},
+    {"class":"Session2","url":"https://www.youtube.com/watch?v=VvL0z75Dl8w"},
+    {"class":"Session3","url":"https://youtu.be/A7SJzgtlG_o"},
+    {"class":"Session4","url":"https://youtu.be/Ao-47b3PCw4"},
+    {"class":"Session5","url":"https://youtu.be/5-f6TbAcEXw"},
+    {"class":"Session6","url":"https://youtu.be/uYnOZ7Vs5PE"},
+    {"class":"Session7","url":"https://youtu.be/TM-D9IozCDQ"},
+    {"class":"Session8","url":"https://youtu.be/Vz-ccbzTn8E"},
+    {"class":"Session9","url":"https://youtu.be/zYB182IOYig"},
+    {"class":"Session10","url":"https://youtu.be/jWFqVCvlphY"},
+    {"class":"Session11","url":"https://youtu.be/aQ9LqnW82Js"},
+    {"class":"Session12","url":"https://youtu.be/-Y7-fNDL-ZE"},
+    {"class":"Session13","url":"https://youtu.be/qJN_fSzlrW0"},
+    {"class":"Session14","url":"https://youtu.be/64F6KjPPjcA"},
+    {"class":"Session15","url":"https://youtu.be/DE7TJVf5p78"},
+    {"class":"Session16","url":"https://youtu.be/Ls2kBTvFtsk"},
+    {"class":"Session17","url":"https://youtu.be/K_tmePsBJU4"},
+    {"class":"Session18","url":"https://youtu.be/AQDDEdfX5GA"},
+    {"class":"Session19","url":"https://youtu.be/6N_POwFqnaY"},
+    {"class":"Session20","url":"https://youtu.be/5IdjjR9LUUU"},
+    {"class":"Session21","url":"https://youtu.be/qucpwzy_PXo"},
+    {"class":"Session22","url":"https://youtu.be/hpXKjWlf1FE"},
+    {"class":"Session23","url":"https://youtu.be/nUacKPM1u4g"},
+    {"class":"Session24","url":"https://youtu.be/7Ez5Iisyd4s"},
+    {"class":"Session25","url":"https://youtu.be/kCWN8HK8S8E"},
+    {"class":"Session26","url":"https://youtu.be/94JrdpfWPEY"},
+    {"class":"Session27","url":"https://youtu.be/1S2qG2TqxQo"},
+    {"class":"Session28","url":"https://youtu.be/zFDk995EaDI"},
+    {"class":"Session29","url":"https://youtu.be/CwEiaRVPwvE"},
+    {"class":"Session30","url":"https://youtu.be/tLnsVDVDmlE"},
+    {"class":"Session31","url":"https://youtu.be/cDI8WDVj_vk"},
+    {"class":"Session32","url":"https://youtu.be/mIx8HNIagZU"},
+    {"class":"Session33","url":"https://youtu.be/fpqJwf62ntg"},
+    {"class":"Session34","url":"https://youtu.be/-kbjZahTfKk"},
+    {"class":"Session35","url":"https://youtu.be/wxWQJUG5P-4"},
+    {"class":"Session36","url":"https://youtu.be/d1d1V2UoFmM"},
+    {"class":"Session37","url":"https://youtu.be/WaIArrR9nVE"},
+    {"class":"Session38","url":"https://youtu.be/NP1lE4Gh4zY"},
+    {"class":"Session39","url":"https://youtu.be/QYOpD-eprcc"},
+    {"class":"Session40","url":"https://youtu.be/BSqelRmQXbU"},
+    {"class":"Session41","url":"https://youtu.be/O2h6UvkcwFU"},
+    {"class":"Session42","url":"https://youtu.be/ueEUZFG08u4"},
+    {"class":"Session43","url":"https://youtu.be/dsDaFtaFHyw"},
+    {"class":"Session44","url":"https://youtu.be/bF2lPEfG8nA"},
+    {"class":"Session45","url":"https://youtu.be/u_t_5YRbvLc"},
+    {"class":"Session46","url":"https://youtu.be/HxSjORaBiBw"},
+    {"class":"Session47","url":"https://youtu.be/Sh3NHHcxnyI"},
+    {"class":"Session48","url":"https://youtu.be/x4rkanOpGJQ"},
+    {"class":"Session49","url":"https://youtu.be/wVdW9AmtnT8"},
+    {"class":"Session50","url":"https://youtu.be/r_bOGKc2_ew"},
+    {"class":"Session51","url":"https://youtu.be/bpMmGaddu04"},
+    {"class":"Session52","url":"https://youtu.be/uL-xZOqY_gU"},
+    {"class":"Session53","url":"https://youtu.be/i2bQUKBPRR8"},
+    {"class":"Session54","url":"https://youtu.be/ZLoICUcDzb8"},
+    {"class":"Session55","url":"https://youtu.be/vi5EevOlNHo"},
+    {"class":"Session56","url":"https://youtu.be/TigfaWPvkO8"},
+    {"class":"Session57","url":"https://youtu.be/dYmdurLxMAo"},
+    {"class":"Session58","url":"https://youtu.be/48OCBrHGOw4"},
+    {"class":"Session59","url":"https://youtu.be/ZPeFKy1aMzA"},
+    {"class":"Session60","url":"https://youtu.be/JAwakxCq63s"},
+    {"class":"Session61","url":"https://youtu.be/YBddPCxSeps"},
+    {"class":"Session62","url":"https://youtu.be/AIwZx2QK-Y4"},
+    {"class":"Session63","url":"https://youtu.be/B53g1bvqu3w"},
+    {"class":"Session64","url":"https://youtu.be/tKQGyS2H-xw"},
+    {"class":"Session65","url":"https://youtu.be/X_qzANxf8vY"},
+    {"class":"Session66","url":"https://youtu.be/8IzzLwx3EEQ"},
+    {"class":"Session67","url":"https://youtu.be/uwP_mnsJuf4"},
+    {"class":"Session68","url":"https://youtu.be/W7m0mu3zdKQ"},
+    {"class":"Session69","url":"https://youtu.be/r7G4px_juyU"},
+    {"class":"Session70","url":"https://youtu.be/KzwFfDnf-og"},
+    {"class":"Session71","url":"https://youtu.be/M1Svb95PYwk"},
+    {"class":"Session72","url":"https://youtu.be/C1RhpS37EHQ"},
+    {"class":"Session73","url":"https://youtu.be/WcWa6sEf0IQ"},
+    {"class":"Session74","url":"https://youtu.be/j7P1N6yNygA"},
+    {"class":"Session75","url":"https://youtu.be/e1IuEDiUrKE"},
+    {"class":"Session76","url":"https://youtu.be/eJqLfrbH_h0"},
+    {"class":"Session77","url":"https://youtu.be/xkZovAzxAsA"},
+]
+
+var main=document.getElementById("main-container")
+var input=document.getElementById("new")
+var btn=document.getElementById("add")
+btn.addEventListener("click",add)
+var value=" "
+len=link.length
+refresh()
+function refresh(){
+    for(let i=0;i<len;i++)
+{
+    // console.log(link[i].class,link[i].url)
+    value+="<a href="+`${link[i].url}`+">"+`${link[i].class}`+"</a><br>"
+}
+main.innerHTML=value
+}
+
+function add(){
+    var merge={"class":'Session'+`${len+1}`,"url":`${input.value}`}
+   link=[...link,merge]
+   console.log(link);
+   refresh()
+}
